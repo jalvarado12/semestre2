55f2e38f44d7cd3f04d89bf08677944386c579fb
jalvarado12 <105568949+jalvarado12@users.noreply.github.com>
2022-09-20 12:07:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@
 
 int main(){
 	vector3d myc;
+	vector3d david;
+	vector3d awa;
 	myc[0]=1;
 	myc[1]=2;
 	myc[2]=3;
 	std::cout<<myc<<"\n ";
+	david[0]=2;
+	david[1]=7;
+	david[2]=-3.4;
+	std::cout<<david<<"\n ";
+	awa=david*5;
 	
-	myc[0];
+	std::cout<<awa<<"\n ";
 	
 	return 0;
 }
