f42043fa2612d66e81cd4ac28c3e7bed5fde9812
jalvarado12 <105568949+jalvarado12@users.noreply.github.com>
2022-09-20 12:08:54 -0500

    Update vector3d.cpp

--- vector3d.cpp
+++ vector3d.cpp
@@ -3,28 +3,26 @@
 vector3d::vector3d(){
 	vec=new double[3];
 }
-
+//-----------------------------------------------------------------------------------------------
 vector3d::~vector3d(){
 	delete[] vec;
 }
-
+//-----------------------------------------------------------------------------------------------
 vector3d& vector3d::operator=(const vector3d &rhs){
 	vec[0]=rhs.vec[0];
 	vec[1]=rhs.vec[1];
 	vec[2]=rhs.vec[2];
 	return *this;
 }
-
-
+//-----------------------------------------------------------------------------------------------
 double &vector3d::operator[]( int i ) {
 	return vec[i];
 }
+//-----------------------------------------------------------------------------------------------
 double vector3d::operator[](int i) const{
 	return vec[i];
 }
-
-
-
+//-----------------------------------------------------------------------------------------------
 vector3d operator*(const vector3d& v , double c){
 	vector3d v1;
 	v1[0]=v[0]*c;
@@ -32,7 +30,7 @@ vector3d operator*(const vector3d& v , double c){
 	v1[2]=v[2]*c;
 	return v1;
 }
-
+//-----------------------------------------------------------------------------------------------
 vector3d operator*(double c , const vector3d& v){
 	vector3d v1;
 	v1[0]=v[0]*c;
@@ -40,7 +38,12 @@ vector3d operator*(double c , const vector3d& v){
 	v1[2]=v[2]*c;
 	return v1;
 }
-
+//-----------------------------------------------------------------------------------------------
+double operator *( const vector3d & v1 , const vector3d & v2 ){
+    return (v1[0]*v2[0])+ (v1[1]*v2[1])+ (v1[2]*v2[2]);
+    
+}
+//-----------------------------------------------------------------------------------------------
 std::ostream& operator<<(std::ostream &os, vector3d &a){
 	 os << "(";
 	 for(int i = 0; i < 2; i++){
@@ -49,3 +52,4 @@ std::ostream& operator<<(std::ostream &os, vector3d &a){
   	os << a[2] << ")";
   	return os;
 }
+//-----------------------------------------------------------------------------------------------
